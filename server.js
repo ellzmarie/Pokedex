843d17bd7e0f9e2a835df6c71d1e3fa033c09c70
ellzmarie <elliematutisdigal@gmail.com>
2022-10-22 17:18:19 -0700

    updates css

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express()
 
 app.use(express.urlencoded({extended:false}))
 app.use(methodOverride('_method'));
-app.use(express.static('/public'));
+app.use(express.static('public'));
 
 require('dotenv').config()
 
